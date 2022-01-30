b4ce92ffa396c5466e1d6f0be91aced18b726566
Josh Lind <joshuaklind@gmail.com>
2022-01-29 22:21:45 -0800

    comms examples

--- doubleTrouble/wifi_client/wifi_client.ino
+++ doubleTrouble/wifi_client/wifi_client.ino
@@ -1,45 +1,46 @@
 /*  Connects to the home WiFi network
- *  Asks some network parameters
- *  Sends and receives message from the server in every 2 seconds
- *  Communicates: wifi_server_01.ino
- */ 
-#include <SPI.h>
+    Asks some network parameters
+    Sends and receives message from the server in every 2 seconds
+    Communicates: wifi_server_01.ino
+*/
 #include <ESP8266WiFi.h>
 
-byte ledPin = LED_BUILTIN;
 char ssid[] = "FreshMesh";           // SSID of your home WiFi
 char pass[] = "freshmesh";            // password of your home WiFi
 
-unsigned long askTimer = 0;
 
-IPAddress server(192,168,0,100);       // the fix IP address of the server
+IPAddress server(192, 168, 43, 58);    // the fix IP address of the server
+// IPAddress ip(192, 168, 43, 27);
+// IPAddress gateway(192, 168, 43, 129);
+// IPAddress subnet(255, 255, 255, 0);
 WiFiClient client;
 
 void setup() {
   Serial.begin(115200);               // only for debug
+  // WiFi.config(ip, gateway, subnet);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);             // connects to the WiFi router
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+
+  if (client.connect(server, 80)) {
+    // Connection to the server
+    Serial.println("Connected");
+  } else {
+    Serial.println("Can't connect");
   }
-/*  Serial.println("Connected to wifi");
-  Serial.print("Status: "); Serial.println(WiFi.status());    // Network parameters
-  Serial.print("IP: ");     Serial.println(WiFi.localIP());
-  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
-  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
-  Serial.print("SSID: "); Serial.println(WiFi.SSID());
-  Serial.print("Signal: "); Serial.println(WiFi.RSSI());*/
-  pinMode(ledPin, OUTPUT);
 }
 
 void loop () {
-  client.connect(server, 80);   // Connection to the server
-  digitalWrite(ledPin, LOW);    // to show the communication only (inverted logic)
-  Serial.println(".");
-  client.println("Hello server! Are you sleeping?\r");  // sends the message to the server
-  String answer = client.readStringUntil('\r');   // receives the answer from the sever
-  Serial.println("from server: " + answer);
-  client.flush();
-  digitalWrite(ledPin, HIGH);
-  delay(2000);                  // client will trigger the communication after two seconds
+  if (!client.connected()) {
+    if (client.connect(server, 80)) {
+      // Connection to the server
+      Serial.println("RE - Connected");
+    } else {
+      Serial.println("Can't connect");
+    }
+  }
+
+  if (client.connected()) {
+      client.println("GET /takephoto/1 HTTP/1.0");  // sends the message to the server
+    Serial.println("ok");
+  }
 }
--- doubleTrouble/wifi_server/wifi_server.ino
+++ doubleTrouble/wifi_server/wifi_server.ino
@@ -1,53 +1,73 @@
 /*  Connects to the home WiFi network
- *  Asks some network parameters
- *  Starts WiFi server with fix IP and listens
- *  Receives and sends messages to the client
- *  Communicates: wifi_client_01.ino
- */
-#include <SPI.h>
+    Asks some network parameters
+    Starts WiFi server with fix IP and listens
+    Receives and sends messages to the client
+    Communicates: wifi_client_01.ino
+*/
 #include <ESP8266WiFi.h>
 
-byte ledPin = LED_BUILTIN;
 char ssid[] = "FreshMesh";           // SSID of your home WiFi
 char pass[] = "freshmesh";            // password of your home WiFi
-WiFiServer server(80);                    
+WiFiServer server(80);
 
-IPAddress ip(192, 168, 0, 100);            // IP address of the server
-IPAddress gateway(192,168,0,1);           // gateway of your network
-IPAddress subnet(255,255,255,0);          // subnet mask of your network
+IPAddress ip(192, 168, 43, 58);            // IP address of the server
+IPAddress gateway(192, 168, 43, 129);        // gateway of your network
+IPAddress subnet(255, 255, 255, 0);       // subnet mask of your network
 
 void setup() {
   Serial.begin(115200);                   // only for debug
+  pinMode(LED_BUILTIN, OUTPUT);
+
   WiFi.config(ip, gateway, subnet);       // forces to use the fix IP
+  WiFi.mode(WIFI_AP);
   WiFi.begin(ssid, pass);                 // connects to the WiFi router
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) { // print "." until connected to wifi
+    delay(50);
     Serial.print(".");
-    delay(500);
   }
-  server.begin();                         // starts the server
-/*  Serial.println("Connected to wifi");
+  Serial.println("Connected to wifi");
   Serial.print("Status: "); Serial.println(WiFi.status());  // some parameters from the network
   Serial.print("IP: ");     Serial.println(WiFi.localIP());
   Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
   Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
   Serial.print("SSID: "); Serial.println(WiFi.SSID());
   Serial.print("Signal: "); Serial.println(WiFi.RSSI());
-  Serial.print("Networks: "); Serial.println(WiFi.scanNetworks());*/
-  pinMode(ledPin, OUTPUT);
+  Serial.print("Networks: "); Serial.println(WiFi.scanNetworks());
+
+  // Starts the server
+  server.begin();
+  Serial.println("Server started");
 }
 
+//Moves servomotor to activate the remote control
+void takePhoto() {
+    delay(1000);
+}
+
+WiFiClient client;
+
 void loop () {
-  WiFiClient client = server.available();
+  if(!client)
+    client = server.available();
   if (client) {
+    Serial.println("Client connected");
     if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
-      Serial.println(".");
+      Serial.println("client is connected");
       String request = client.readStringUntil('\r');    // receives the message from the client
-      Serial.print("From client: "); Serial.println(request);
-      client.flush();
-      client.println("Hi client! No, I am listening.\r"); // sends the answer to the client
-      digitalWrite(ledPin, HIGH);
+      Serial.print("From client: ");
+      Serial.println(request);
+
+      int val;
+      //filter the client request
+      if (request.indexOf("/takephoto/1") != -1) { 
+        takePhoto();
+        Serial.println("taking photo");
+      } else {
+        Serial.print("invalid request: ");
+        Serial.println(request);
+      }
+      
+      client.stop();
     }
-    client.stop();                // tarminates the connection with the client
   }
 }
